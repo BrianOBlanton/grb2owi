7becd09c8f506b5efe3b00b1c1af310691f2bd4a
Brian Blanton <Brian_Blanton@Renci.Org>
2018-11-16 10:14:55 -0500

    import of files

---
+++ grb2owi.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+
+Usage()
+{
+cat <<-ENDOFMESSAGE
+
+Usage: grb2owi.sh <options> gribfiles
+
+Version 0.1
+
+grb2owi converts grib2 files in standard NCEP projections to OWI files (221, 222) 
+on an equidistant lonlat grid.  It uses wgrib2 to extract pressure, u10 and v10 
+from a sequence of grib2 files, interpolate from the grib2 grid to the user-defined 
+grid, write out each time step (snap) to OWI text files, and concatenate the 
+results into consolidated 221,222 files.  
+
+By default, it assumes that:
+        * UVP records are in each grib2 file
+        * Each file contains only one time level
+        * The files are named in a sequence that represents strictly increasing times, 
+        *       such that the wildcard expansion of the name is in the proper time order;
+        *       if this is NOT true, then the user must put the files in the correct 
+        *       order on the commandline explicitly, e.g.,  ... file1.grb2 file2.grb2 ...
+        * The default variable names looked for in the grib2 files are: PRES UGRD VGRD
+        * The variables'' vertical levels are assumed to be "mean sea level", 
+        *       "10 m above ground", and "10 m above ground", respectively.
+        * The default variable:level combinations are thus:
+        *    --presname "PRES:mean sea level"
+        *    --ugrdname "UGRD:10 m above ground"
+        *    --vgrdname "VGRD:10 m above ground"
+
+Options:
+        --ugrdname : name of e/w wind speed variable in grib file (def="UGRD:10 m above ground")
+        --vgrdname : name of n/s wind speed variable in grib file (def="VGRD:10 m above ground")
+        --presname : name of MSL pressure variable in grib file (def="PRES:mean sea level")
+        --lon1     : longitude of lower left corner of lonlat box (def=276)
+        --nlon     : number of points in lon direction (def=100)
+        --dlon     : longitude increment for interpolation (def=.0267)
+        --lat1     : latitude of lower left corner of lonlat box (def=24)
+        --nlat     : number of points in lat direction (def=100)
+        --dlat     : latitude increment for interpolation (def=.0267)
+
+Outputs:
+        f.221, f.222 in OWI format
+
+Notes:
+        *) the grib interpolation process will convert longitudes to negative-west 
+          	convention if lon1 > 0
+        *) user should use wgrib2 to investigate grib grid so appropriately set 
+         	equidistant grid parameters
+
+Examples:
+        1) Convert files matching wildcard to OWI, using default regular grid parameters:
+                grb2owi.sh sfc_2016100620_*.grb2
+        2) Convert files matching wildcard to OWI, with 200 points in each direction, 
+           starting at lower-left corner == (278,22)
+                grb2owi.sh --nlon 200 -nlan 200 -lon1 278 -lat1 22 sfc_2016100620_*.grb2
+        3) Convert three files explicitly named on the commandline, using defaults: 
+                grb2owi.sh file234.grb2 file9873.grb2 file1.grb2
+        4) Convert files using a non-default pressure varuable:level: 
+                grb2owi.sh --presname="PRMSL:mean sea level" nam_2016100620_*.grb2
+                
+                
+Brian Blanton
+Renaissance Computing Institute			
+Oct 2016
+
+ENDOFMESSAGE
+}
+
+#####
+#####  parse input arguments
+#####
+if [ "$#" -eq 0 ] ; then
+        Usage
+        exit 0
+fi
+
+GETOPT='getopt'
+if [[ `uname` -eq "Darwin" ]]; then 
+        GETOPT='/usr/local/Cellar/gnu-getopt/1.1.6/bin/getopt'
+fi
+
+OPTS=`$GETOPT -o v --long ugrdname:,vgrdname:,presname:,lon1:,nlon:,dlon:,lat1:,nlat:,dlat: -n 'parse-options' -- "$@"`
+
+if [ $? != 0 ]
+then
+    echo "Failed to parse commandline."
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+ugrdname="UGRD:10 m above ground"
+vgrdname="VGRD:10 m above ground"
+presname="PRES:mean sea level"
+LON1=276
+NLON=100
+DLON=.0267
+LAT1=24
+NLAT=100
+DLAT=.0267
+VERBOSE=false
+
+while true ; do
+    case "$1" in
+        -v) VERBOSE=true; shift;;
+        --lon1) LON1=$2; shift 2;;
+        --nlon) NLON=$2; shift 2;;
+        --dlon) DLON=$2; shift 2;;
+        --lat1) LAT1=$2; shift 2;;
+        --nlat) NLAT=$2; shift 2;;
+        --dlat) DLAT=$2; shift 2;;
+        --ugrdname) ugrdname=$2; shift 2;;
+        --vgrdname) vgrdname=$2; shift 2;;
+        --presname) presname=$2; shift 2;;
+        --) shift; break;;
+    esac
+done
+
+if [ "$VERBOSE" == true ]; then 
+        echo VERBOSE = $VERBOSE
+        echo LON1 = $LON1
+        echo NLON = $NLON
+        echo DLON = $DLON
+        echo LAT1 = $LAT1
+        echo NLAT = $NLAT
+        echo DLAT = $DLAT
+        echo Pressure Variable Name = $presname
+        echo UGRD Variable Name = $ugrdname
+        echo VGRD Variable Name = $vgrdname
+        echo "Remaining Args:"
+        echo $@
+fi
+
+#####
+#####  done parsing input arguments
+#####
+
+#WGRIB2="/Users/bblanton/bin/wgrib2"
+WGRIB2=`which wgrib2`
+if [ $? -eq 0 ]; then
+        if [ "$VERBOSE" == true ]; then 
+                echo "Found wgrib2 at $WGRIB2"
+        fi
+else
+        echo "Cant find wgrib2"
+        exit 1
+fi
+
+FirstFile=$1
+nfiles="$#"
+echo "$nfiles grb2 files to process.  Go get coffee..."
+
+if [ $nfiles -lt 2 ]; then
+        LastFile=$FirstFile
+else
+        LastFile=${!nfiles}
+fi
+echo FirstFile=$FirstFile
+echo LastFile =$LastFile
+
+
+#####
+##### Check for variables in first grib file
+#####
+
+tt=`$WGRIB2  -match "$presname" "$FirstFile" `
+if [ -z "$tt" ] ; then 
+	echo "Pressure variable ($presname) not found in $FirstFile"
+	exit 1
+fi
+tt=`$WGRIB2  -match "$ugrdname" "$FirstFile" `
+if [ -z "$tt" ] ; then 
+	echo "UGRD variable ($ugrdname) not found in $FirstFile"
+	exit 1
+fi
+tt=`$WGRIB2  -match "$vgrdname" "$FirstFile" `
+if [ -z "$tt" ] ; then 
+	echo "VGRD variable ($vgrdname) not found in $FirstFile"
+	exit 1
+fi
+		
+#####
+##### build header line
+#####
+
+t1=`$WGRIB2 -d 1 -end_FT  "$FirstFile"  | cut -d = -f 2`
+t1=`echo ${t1:0:12}`
+if [ "$VERBOSE" == true ]; then echo "Starting Time = $t1"; fi
+t2=`$WGRIB2 -d 1 -end_FT  "$LastFile"  | cut -d = -f 2`
+t2=`echo ${t2:0:12}`
+if [ "$VERBOSE" == true ]; then echo "Ending Time   = $t2"; fi
+
+IFS=';'
+hline='Oceanweather WIN/PRE Format                        %12s     %12s\n'
+printf $hline "$t1" "$t2" > h_main.txt
+unset IFS
+echo "OWI Header line:"
+cat h_main.txt
+
+
+#####
+##### build individual snap files
+#####
+
+let c=10000
+for f in "$@"
+do
+        dd=`$WGRIB2  -d 1 -end_FT  $f |awk 'BEGIN { FS = "=" } ; { print $2 }' `
+        echo "Processing $f @ $dd ..."
+        sh grb2owi_snap.sh $LON1 $NLON $DLON $LAT1 $NLAT $DLAT $f "$presname" "$ugrdname" "$vgrdname"
+        if [ $? != 0 ]; then
+        	echo grb2owi_snap.sh  failed with these parameters:
+        	echo "$LON1 $NLON $DLON $LAT1 $NLAT $DLAT $f $presname $ugrdname $vgrdname"
+        	exit 1
+        fi
+		mv p.txt p.txt.$c
+        mv uv.txt uv.txt.$c
+        let "c++" 
+done
+
+#####
+##### build final file
+#####
+
+cat h_main.txt > f.221
+        cat p.txt.* >> f.221
+cat h_main.txt > f.222
+        cat uv.txt.* >> f.222
+
+rm *.txt.1* h_main.txt
+
---
+++ grb2owi_snap.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+if [ "$#" -eq 10 ]
+then
+        lon1=$1
+        nlon=$2
+        dlon=$3
+        lat1=$4
+        nlat=$5
+        dlat=$6
+        INgrb=$7
+        pname=$8
+        uname=$9
+        vname=${10}
+else
+        echo "Usage: grb2owi_snap.sh lon1 nlon dlon lat1 nlat dlat grib2file presname uname vname"
+        exit 1
+fi
+
+# test lon1 for >0, sub 360 if so (assumes western hemis).
+if (( $(bc <<< "$lon1 > 0") ))
+then
+        neglon=`echo $lon1-360 | bc`
+else
+        neglon=$lon1
+fi
+
+#echo lon="$lon1:$nlon:$dlon" lat="$lat1:$nlat:$dlat" 
+
+# reduce to uvp
+wgrib2 $INgrb -match "($pname|$uname|$vname)" -grib temp.grb &>/dev/null
+
+# convert p from Pa to mb
+wgrib2 temp.grb -if "$pname" -rpn "100:/" -fi -grib_out temp2.grb  &>/dev/null
+
+# interp to equidistant grid
+wgrib2 temp2.grb -set_grib_type same -new_grid_winds earth -new_grid latlon $lon1:$nlon:$dlon $lat1:$nlat:$dlat   temp3.grb  &>/dev/null
+#wgrib2 temp3.grb -gridout ll.dat
+
+# time interpolation
+# http://www.cpc.ncep.noaa.gov/products/wesley/wgrib2/time_interpolation.html
+
+wgrib2 temp3.grb -match "$pname" -no_header -text_fmt '%9.4f' -text_col 8 -text p.txt &>/dev/null
+wgrib2 temp3.grb -match "$uname" -no_header -text_fmt '%9.4f' -text_col 8 -text u.txt &>/dev/null
+wgrib2 temp3.grb -match "$vname" -no_header -text_fmt '%9.4f' -text_col 8 -text v.txt &>/dev/null
+
+# add eol just in case the last line has fewer than 8 columns
+sed -i -e '$a\' p.txt
+sed -i -e '$a\' u.txt
+sed -i -e '$a\' v.txt
+cat u.txt v.txt > uv.txt
+
+# add a blank column on text files
+#0        1         2         3         4         5         6         7         8
+#12345678901234567890123456789012345678901234567890123456789012345678901234567890
+#Oceanweather WIN/PRE Format                        200809030500     200809101650
+#iLat= 601iLong= 601DX=0.0500DY=0.0500SWLat=20.00000SWLon=-90.0000DT=200809030500
+# 1012.6060 1012.6050 1012.6050 1012.6050 1012.6050 1012.6050 1012.6040 1012.6040
+awk '{print " "$0}' p.txt > p.txt2
+awk '{print " "$0}' uv.txt > uv.txt2
+
+# get time for tline
+# need to check here if this is an analysis file, 
+# in which case use the verificaiton time
+# otherwise, use the forecast time
+#ftype=`wgrib2 -d 1 -ftime  $INgrb  | cut -d : -f 3`
+#if [ "$ftype" = "anl" ]; then
+        dd=`wgrib2  -d 1 -end_FT  temp.grb  |awk 'BEGIN { FS = "=" } ; { print $2 }' `
+        dd=`echo ${dd:0:12}`
+#else
+#        dd=`wgrib2  -d 1 -end_FT temp.grb  | cut -d = -f 2`
+#        dd=`echo ${dd:0:12}`
+#fi
+tline="iLat=%4diLong=%4dDX=%6.4fDY=%6.4fSWLat=%8.5fSWLon=%8.4fDT=%12d\n"
+printf $tline "$nlat" "$nlon" "$dlat" "$dlon" "$lat1" "$neglon" "$dd"> h.txt
+
+# add headers to txt snaps
+cat h.txt p.txt2 > p.txt3
+cat h.txt uv.txt2 > uv.txt3
+
+#cleanup
+rm -f temp.grb temp2.grb temp3.grb p.txt2 uv.txt uv.txt2 p.txt u.txt v.txt *.txt-e h.txt
+
+mv p.txt3 p.txt
+mv uv.txt3 uv.txt
+
